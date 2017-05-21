f5c60d3676e04cdf76aaec9510c00721863c388a
Jen <jtrudell@gmail.com>
2017-05-21 12:12:22 -0500

    Some solutions to the array problems

--- app/arrays.js
+++ app/arrays.js
@@ -2,15 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum: function(arr) {
-
+    return arr.reduce(function(sum, item) {
+      return sum + item;
+    }, 0);
   },
 
   remove: function(arr, item) {
-
+    return arr.filter(function(remove) {
+      return item !== remove;
+    });
   },
 
   removeWithoutCopy: function(arr, item) {
@@ -18,31 +22,42 @@ exports.arraysAnswers = {
   },
 
   append: function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+    var tail = arr.splice(index);
+    return arr.concat(item, tail);
   },
 
   count: function(arr, item) {
-
+    return arr.reduce(function(sum, x) {
+      var num = sum;
+      if (x === item) {
+        num++;
+      }
+      return num;
+    }, 0);
   },
 
   duplicates: function(arr) {
@@ -50,10 +65,18 @@ exports.arraysAnswers = {
   },
 
   square: function(arr) {
-
+    return arr.map(function(item) {
+      return item * item;
+    });
   },
 
   findAllOccurrences: function(arr, target) {
-
+    var found = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        found.push(i);
+      }
+    }
+    return found;
   }
 };
