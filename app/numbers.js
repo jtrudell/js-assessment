d31729c416c1d80fbb59f565411f3cb0663e6c5b
Jen <jtrudell@gmail.com>
2017-08-14 12:31:58 -0500

    numbers and binary stuff

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    var binAry = this.convertToBinary(num).split('');
+    return Number.parseInt(binAry.reverse()[bit - 1], 10);
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    var binNum = num.toString(2);
+    if (binNum.length !== 8) {
+      binNum = '0'.concat(binNum);
+    }
+    return binNum;
   },
 
   multiply: function(a, b) {
-
+    return a * b;
   }
 };
